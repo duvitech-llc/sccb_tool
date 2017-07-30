c8ad03e0d0ddc73a98a88ba4719e3bf8d49bff09
George Vigelette <gvigelet@duvitech.com>
2017-07-30 17:02:15 -0400

    added device address

--- sccb_tool.c
+++ sccb_tool.c
@@ -5,13 +5,24 @@
 #include <unistd.h>
 #include <getopt.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
+
+enum xfer_state{
+	READ = 0, 
+	WRITE = 1,
+	NOTSET = -1};
+
+enum reg_size{
+	BYTE_REG = 0, 
+	SHORT_REG = 1,
+	WORD_REG = 2,
+	NOTSET_REG = -1};
 
 int file = -1;
-int adapter_nr = 2; /* probably dynamically determined */
-char filename[20];
-int parsed = -1;
-int readwrite = -1;
+char filename[20] = {0};
 
 static struct option long_options[] = {
     {"write", required_argument, 0,'w'},
@@ -25,43 +36,27 @@ static void display_menu(void){
     printf("\nSCCB Tool\n");
     printf("\nAdapter: 0-9\n");
     printf("\nRegister Size: w, s, b\n");    
-    printf("\nsccb -w:r <adapter> <reg_sz> <reg> <val>\n\n");
+    printf("\nsccb -w:r <adapter> <dev_addr> <reg_size> <reg> <val>\n\n");
 
 
 
 }
 
-static void GetArgsList (int argc, char *argv[], int* o_argc, char*** o_argv)
-{
-    char** ret = NULL;
-    int i;
-    int count = 0;
+static int write_sccb_register(int register_address, int register_value, enum reg_size data_width){
+	int ret = 0;
 
-    for (i = optind - 1; i < argc ; ++i)
-    {
-        if (argv[i][0]=='-')
-        {
-            break;
-        }
-        else
-        {
-            if (NULL == ret)
-            {
-                ret = &argv[i];
-            }
-            count++;
-        }
-    }
 
-    optind = i - 1;
-
-    if (o_argc)
-    {
-        *o_argc = count;
-    }
-
-    *o_argv = ret;
+	return ret;
 }
+
+
+static int read_sccb_register(int register_address, int* register_value, enum reg_size data_width){
+	int ret = 0;
+
+
+	return ret;
+}
+
 
 int main(int argc, char *argv[]){
 	int arg = 0;
@@ -71,6 +66,15 @@ int main(int argc, char *argv[]){
     int index = 0;
     char * next;
 
+	enum xfer_state readwrite = NOTSET;
+	enum reg_size datawidth = NOTSET_REG;
+	int adapter_nr = 2; 
+	int dev_addr = 0x00;
+	int reg_addr = 0x00;
+	int reg_val = 0x00;
+	
+	int parsed = -1;
+
     printf("argc: %d\n", argc);
 
     while (arg != -1) {
@@ -79,7 +83,78 @@ int main(int argc, char *argv[]){
         	case 'w':
         	{     	
         		printf("Write Sensor\n"); 
-        		readwrite = 1;
+        		readwrite = WRITE;
+	            index = optind-1;
+	
+        		int n = 5;		
+        		while(n>0){
+
+	            	printf("index: %d\n", index);
+	            	if(index>=argc){
+	            		printf("Error not enough args\n");
+        				goto error_handler;
+	            	}
+
+        			next = strdup(argv[index]);
+        			switch(n){
+        				case 5:
+        					adapter_nr = atoi(next);
+	            			printf("Adapter: %d\n", adapter_nr);
+        				break;
+        				case 4:
+        					dev_addr = (int)strtol(next, NULL, 0);
+	            			printf("Device Address: 0x%02X\n", dev_addr);
+        				break;
+        				case 3:
+        					switch((char)*next){
+        						case 'w':
+        						case 'W':
+									printf("32-bit size selected\n");
+									datawidth = WORD_REG;
+        						break;
+        						case 's':
+        						case 'S':
+									printf("16-bit size selected\n");
+									datawidth = SHORT_REG;
+        						break;
+        						case 'b':
+        						case 'B':
+									printf("8-bit size selected\n");
+									datawidth = BYTE_REG;
+        						break;        						
+        						default:
+									printf("Invalid size opiton\n");
+									datawidth = NOTSET_REG;
+        						break;
+        					}
+	            			
+	            			printf("Reg_Size: %s\n", next);
+
+        				break;
+        				case 2:
+        					reg_addr = (int)strtol(next, NULL, 0);
+	            			printf("Reg: 0x%08X\n", reg_addr);
+        				break;
+        				case 1:
+	            			reg_val = (int)strtol(next, NULL, 0);
+	            			printf("Val: 0x%02X\n", reg_val);
+        				break;
+        				default:
+        					printf("default handler error!!!!\n");
+        					goto error_handler;
+        				break;
+        			}
+            		
+            		n--;
+            		index++;
+            		
+				}
+        	}
+        	break;
+        	case 'r':
+        	{
+        		printf("Read Sensor\n"); 
+        		readwrite = READ;
 	            index = optind-1;
 	
         		int n = 4;		
@@ -98,82 +173,29 @@ int main(int argc, char *argv[]){
 	            			printf("Adapter: %d\n", adapter_nr);
         				break;
         				case 3:
-        					switch((char)*next){
-        						case 'w':
-        						case 'W':
-									printf("32-bit size selected\n");
-        						break;
-        						case 's':
-        						case 'S':
-									printf("16-bit size selected\n");
-        						break;
-        						case 'b':
-        						case 'B':
-									printf("8-bit size selected\n");
-        						break;        						
-        						default:
-									printf("Invalid size opiton\n");
-        						break;
-        					}
-	            			
-	            			printf("Reg_Size: %s\n", next);
-
-        				break;
-        				case 2:
-	            			printf("Reg: 0x%08X\n", (int)strtol(next, NULL, 0));
-        				break;
-        				case 1:
-	            			printf("Val: 0x%02X\n", (int)strtol(next, NULL, 0));
-        				break;
-        				default:
-        					printf("default handler error!!!!\n");
-        					goto error_handler;
-        				break;
-        			}
-            		
-            		n--;
-            		index++;
-            		
-				}
-        	}
-        	break;
-        	case 'r':
-        	{
-        		printf("Read Sensor\n"); 
-        		readwrite = 0;
-	            index = optind-1;
-	
-        		int n = 3;		
-        		while(n>0){
-
-	            	printf("index: %d\n", index);
-	            	if(index>=argc){
-	            		printf("Error not enough args\n");
-        				goto error_handler;
-	            	}
-
-        			next = strdup(argv[index]);
-        			switch(n){
-        				case 3:
-        					adapter_nr = atoi(next);
-	            			printf("Adapter: %d\n", adapter_nr);
+        					dev_addr = (int)strtol(next, NULL, 0);
+	            			printf("Device Address: 0x%02X\n", dev_addr);
         				break;
         				case 2:
         					switch((char)*next){
         						case 'w':
         						case 'W':
 									printf("32-bit size selected\n");
+									datawidth = WORD_REG;
         						break;
         						case 's':
         						case 'S':
 									printf("16-bit size selected\n");
+									datawidth = SHORT_REG;
         						break;
         						case 'b':
         						case 'B':
 									printf("8-bit size selected\n");
+									datawidth = BYTE_REG;
         						break;        						
         						default:
 									printf("Invalid size opiton\n");
+									datawidth = NOTSET_REG;
         						break;
         					}
 	            			
@@ -181,7 +203,8 @@ int main(int argc, char *argv[]){
 
         				break;
         				case 1:
-	            			printf("Reg: 0x%08X\n", (int)strtol(next, NULL, 0));
+        					reg_addr = (int)strtol(next, NULL, 0);
+	            			printf("Reg: 0x%08X\n", reg_addr);
         				break;
         				default:
         					printf("default handler error!!!!\n");
@@ -209,19 +232,40 @@ int main(int argc, char *argv[]){
 	}
 
 	if(parsed){
+		/* open file Handle */
+		sprintf( filename, "/dev/i2c-%d", adapter_nr );
+		if ((file = open(filename, O_RDWR)) < 0) {
+		    /* ERROR HANDLING: you can check errno to see what went wrong */
+		    perror("Failed to open the i2c bus");
+		    exit(1);
+		}
 
 		printf("Run function ");
-		if(readwrite){
+		if(readwrite == WRITE){
 			printf("write \n");
+			if(write_sccb_register(reg_addr, reg_val, datawidth) == -1){
+				printf("Failed to write register\n");
+
+			}
 		}else{
 			printf("read \n");
+			reg_val = 0;
+			if(read_sccb_register(reg_addr, &reg_val, datawidth) == -1){
+				printf("Failed to read register\n");
+			}
 		}
+
+		/* close file handle */
+		if(file > 0){
+			close(file);
+		}
+
 		return 0;
+	}else{
+		printf("Failed to parse parameters\n");
 	}
 
-error_handler:
-        		
+error_handler:        		
     display_menu();
-
 	return ret;
 }
